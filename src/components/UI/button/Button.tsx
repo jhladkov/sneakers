aa01b488ef312bcb27f04fc775bbf28dbdeda3c2
Zhenua <zhenua.gladkovv@gmail.com>
2021-11-02 22:12:08 +0200

    app

--- src/components/UI/button/Button.tsx
+++ src/components/UI/button/Button.tsx
@@ -1,14 +1,35 @@
 import React, {FC} from 'react';
+import './button.scss'
 
 interface ButtonProps {
     className: string;
-    text:string;
-
+    classSvg?: string
+    text: string;
+    func?: any
 }
 
-const Button:FC<ButtonProps> = ({className,text}) => {
+const Button: FC<ButtonProps> = ({className, text, classSvg,func}) => {
     return (
-        <button className={className}>{text}</button>
+        <button className={className} onClick={func}>
+            <div>
+                {
+                    classSvg && classSvg === 'go-back'
+                        ? <svg id="icon-arrow-left" viewBox="0 0 24 24">
+                            <path d="M12.707 18.293l-5.293-5.293h11.586c0.552 0 1-0.448 1-1s-0.448-1-1-1h-11.586l5.293-5.293c0.391-0.391 0.391-1.024 0-1.414s-1.024-0.391-1.414 0l-7 7c-0.096 0.096-0.168 0.206-0.217 0.324-0.051 0.122-0.076 0.253-0.076 0.383 0 0.256 0.098 0.512 0.293 0.707l7 7c0.391 0.391 1.024 0.391 1.414 0s0.391-1.024 0-1.414z"></path>
+                        </svg>
+                        : null
+                }
+                <span>{text}</span>
+                {
+                    classSvg && classSvg === 'go-buy'
+                        ? <svg id="icon-arrow-right" viewBox="0 0 24 24">
+                            <path d="M11.293 5.707l5.293 5.293h-11.586c-0.552 0-1 0.448-1 1s0.448 1 1 1h11.586l-5.293 5.293c-0.391 0.391-0.391 1.024 0 1.414s1.024 0.391 1.414 0l7-7c0.092-0.092 0.166-0.202 0.217-0.324 0.101-0.245 0.101-0.521 0-0.766-0.049-0.118-0.121-0.228-0.217-0.324l-7-7c-0.391-0.391-1.024-0.391-1.414 0s-0.391 1.024 0 1.414z"></path>
+                        </svg>
+                        : null
+
+                }
+            </div>
+        </button>
     );
 };
 
--- src/components/cart-manager/CartManager.tsx
+++ src/components/cart-manager/CartManager.tsx
@@ -3,6 +3,8 @@ import './cart-manager.scss'
 import CartManagerItem from "../cart-manager-item/CartManagerItem";
 import Title from "../title/Title";
 import {useActions} from "../../hooks/useActions";
+import Button from "../UI/button/Button";
+import {useTypedSelector} from "../../hooks/useTypedSelector";
 
 interface CartManagerProps {
     className?: string
@@ -10,18 +12,52 @@ interface CartManagerProps {
 }
 
 const CartManager: FC<CartManagerProps> = ({className, cartItems}) => {
-    const {addItemToCart} = useActions()
+    const {home} = useTypedSelector(state => state)
+
+    const {addItemToCart, changeStatusMenu} = useActions()
+
     return (
         <div className={`cart ${className}`}>
-            <Title text='Корзина' className='cart-title title'/>
-            {
-              cartItems && cartItems.length > 0
-                    ? cartItems.map((item: any) => {
-                        return <CartManagerItem key={item.id + 0.5} addItemToCart={addItemToCart} cartItems={cartItems} price={item.price} img={item.img} name={item.name} id={item.id}/>
-                    })
-                    : <div>Вы ничего не добавили!</div>
-            }
+            <Title text='Корзина' className='cart-title title min'/>
+            <div className="wrapper">
 
+                <div className="cart-wrapper-item">
+                    {
+                        cartItems && cartItems.length > 0
+                            ? cartItems.map((item: any) => {
+                                return <CartManagerItem key={item.id + 0.5} addItemToCart={addItemToCart}
+                                                        cartItems={cartItems}
+                                                        price={item.price} img={item.img} name={item.name} id={item.id}/>
+                            })
+                            :
+                            <div className='cart-inner'>
+                                <div className="cart-inner-img">
+                                    <img src="./img/cart-img/box.jpg" alt=""/>
+                                </div>
+                                <Title text='Корзина пустая' className='cart-inner-title title min '/>
+                                <p className='cart-inner-text'>
+                                    Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ.
+                                </p>
+                                <Button func={() => changeStatusMenu(false)} className='cart-inner-button button'
+                                        classSvg='go-back' text='Вернуться назад'/>
+                            </div>
+                    }
+                </div>
+                {
+                    cartItems && cartItems.length > 0
+                        ? <div className='cart-pay'>
+                            <div className="cart-pay-info">
+                                <div className="cart-pay-info-block">
+                                    <div className="cart-pay-info-block-title">Итого:</div>
+                                    <div className="cart-pay-info-block-dotted-line"></div>
+                                    <div className="cart-pay-info-block-price price">{home.price}руб.</div>
+                                </div>
+                                <Button className='cart-pay-info-button button' text='Оформить заказ' classSvg='go-buy'/>
+                            </div>
+                        </div>
+                        : null
+                }
+            </div>
         </div>
     );
 };
